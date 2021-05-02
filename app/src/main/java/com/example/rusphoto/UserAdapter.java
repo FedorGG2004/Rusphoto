046778333300943dcd194158e4fb918c34378448
FedorGG2004 <theopotter@yandex.ru>
2021-05-02 15:36:09 +0300

    1

--- app/src/main/java/com/example/rusphoto/UserAdapter.java
+++ app/src/main/java/com/example/rusphoto/UserAdapter.java
@@ -7,11 +7,12 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     ArrayList<User> users;
-
     public UserAdapter(ArrayList<User> users) {
         this.users = users;
     }
@@ -25,6 +26,9 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(UserAdapter.ViewHolder holder, int position) {
         holder.firstName.setText(users.get(position).getName());
+        holder.lastName.setText(users.get(position).getLastname());
+        holder.email.setText(users.get(position).getEmail());
+
 
     }
 
@@ -35,9 +39,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView firstName;
+        public TextView lastName;
+        public TextView email;
         public ViewHolder(View itemView) {
             super(itemView);
             firstName = itemView.findViewById(R.id.first_name);
+            lastName = itemView.findViewById(R.id.last_name);
+            email = itemView.findViewById(R.id.email);
         }
     }
 }
