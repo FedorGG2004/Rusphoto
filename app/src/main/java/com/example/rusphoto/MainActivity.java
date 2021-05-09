27f32c176a063ac31e6bbcd3dbb84d05fb78b25e
FedorGG2004 <theopotter@yandex.ru>
2021-05-09 23:15:45 +0300

    Merge remote-tracking branch 'Rusphoto/master'

--- app/src/main/java/com/example/rusphoto/MainActivity.java
+++ app/src/main/java/com/example/rusphoto/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.rusphoto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.rusphoto.databinding.ActivityMainBinding;
+import com.example.rusphoto.fragments.HomeFragment;
+import com.example.rusphoto.fragments.NotificationFragment;
+import com.example.rusphoto.fragments.SmsFragment;
 import com.example.rusphoto.story.Story;
 import com.example.rusphoto.tesseract.Tesseract;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         binding.button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Tesseract.class);
@@ -26,8 +36,35 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, Story.class);
             startActivity(intent);
         });
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
 
         setContentView(binding.getRoot());
     }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_sms:
+                            openFragment(SmsFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_notifications:
+                            openFragment(NotificationFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
 }
